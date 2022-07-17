fc6b533ad18a33c89547bb323ff72c8ba55507b6
Patrick <ruomungai@gmail.com>
2022-07-17 20:05:34 +0300

    commit tree b3b3914ca304e42c570c5ee4bd779090619d5a31

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentComponent from './components/Form'
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent />,
   document.getElementById('root')
 );
